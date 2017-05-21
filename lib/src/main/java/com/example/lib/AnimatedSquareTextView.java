daffe4c80632cc13b4a57488477512b79ef0499a
Rustam Sinukov <rustik1@yandex-team.ru>
2017-05-21 03:14:11 +0300

    Add configuration via attrs

--- lib/src/main/java/com/example/lib/AnimatedSquareTextView.java
+++ lib/src/main/java/com/example/lib/AnimatedSquareTextView.java
@@ -1,8 +1,11 @@
 package com.example.lib;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
@@ -11,27 +14,67 @@ import static android.view.View.MeasureSpec.EXACTLY;
 
 public class AnimatedSquareTextView extends AppCompatTextView {
 
-    public static final int TRANSITION_TIME_MILLIS = 500;
+    private static final int DEFAULT_TRANSITION_TIME_MILLIS = 500;
+    private static final int DEFAULT_STATIC_TIME_MILLIS = 500;
+    private static final int NO_ITEMS_ID = -1;
 
     public AnimatedSquareTextView(@NonNull Context context) {
         super(context);
-        init(context);
+        init(context, null);
     }
 
     public AnimatedSquareTextView(@NonNull Context context, @NonNull AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init(context, attrs);
     }
 
     public AnimatedSquareTextView(@NonNull Context context, @NonNull AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        init(context, attrs);
     }
 
-    private void init(@NonNull Context context) {
-        final AnimationDrawable bg = (AnimationDrawable) ContextCompat.getDrawable(context, R.drawable.text_bg);
-        bg.setEnterFadeDuration(TRANSITION_TIME_MILLIS);
-        bg.setExitFadeDuration(TRANSITION_TIME_MILLIS);
+    private void init(@NonNull Context context, @Nullable AttributeSet attrs) {
+        final TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.AnimatedSquareTextView, 0, 0);
+        final int transitionDuration;
+        final int staticDuration;
+        final Drawable[] items;
+
+        try {
+            transitionDuration = ta.getInt(
+                    R.styleable.AnimatedSquareTextView_transitionDuration,
+                    DEFAULT_TRANSITION_TIME_MILLIS
+            );
+            staticDuration = ta.getInt(
+                    R.styleable.AnimatedSquareTextView_staticDuration,
+                    DEFAULT_STATIC_TIME_MILLIS
+            );
+            final int itemsArrayId = ta.getResourceId(R.styleable.AnimatedSquareTextView_items, NO_ITEMS_ID);
+            if (itemsArrayId == NO_ITEMS_ID) {
+                items = new Drawable[]{
+                        ContextCompat.getDrawable(context, R.drawable.gradient_2),
+                        ContextCompat.getDrawable(context, R.drawable.gradient_1)
+                };
+            } else {
+                final TypedArray itemsTypedArray = context.getResources().obtainTypedArray(itemsArrayId);
+                try {
+                    items = new Drawable[itemsTypedArray.length()];
+                    for (int i = 0; i < items.length; i++) {
+                        items[i] = itemsTypedArray.getDrawable(i);
+                    }
+                } finally {
+                    itemsTypedArray.recycle();
+                }
+            }
+        } finally {
+            ta.recycle();
+        }
+
+        final AnimationDrawable bg = new AnimationDrawable();
+        for (Drawable item : items) {
+            bg.addFrame(item, staticDuration + transitionDuration);
+        }
+        bg.setEnterFadeDuration(transitionDuration);
+        bg.setExitFadeDuration(transitionDuration);
         setBackgroundDrawable(bg);
     }
 
